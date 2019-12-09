945e7f5faa2543dfa7d04b660ebcc9622235aeca
Manuel Ebert <manuel@1450.me>
2019-12-09 15:45:59 -0800

    Satisfy the linter gods

--- tests/test_snoo.py
+++ tests/test_snoo.py
@@ -2,9 +2,11 @@ from snoo.client import Client
 from snoo import run
 import pytest
 
+
 def test_client():
     client = Client()
     assert client.config is not None
+
 
 def test_cli():
     with pytest.raises(SystemExit):
